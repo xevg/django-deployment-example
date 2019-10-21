3184029f076decc9e81aa52c1d141795c85cd084
Xev <git@schore.org>
2019-10-21 09:40:18 -0400

    initial commit

---
+++ Learning_Templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='mycut')
+def mycut (value, arg):
+    """
+    This cuts out all values of arg from the string
+    """
+
+    return value.replace(arg,'')
+
+
+# register.filter('mycut', mycut)
